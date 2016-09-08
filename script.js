51d2df5c0f2c3c1418cc91f800a204911fe78119
concepsheon <raine20137@gmail.com>
2016-09-08 22:09:15 +0000

    add company profile code

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//control for side menu
 var open = false;
 $("#more-menu").click(function(){
     if(!open){
@@ -11,11 +12,32 @@ $("#more-menu").click(function(){
     }
 });
 
+//when changes made in side menu save button is shown
 $("input[type=checkbox], .user-type").click(function(){
     $("#save-btn").animate({ "top": "0" }, 300);
 });
 
+//when save button is clicked, save button disappears
 $("#save-btn").click(function(){
     $(this).animate({ "top": "-80px" }, 600);
 });
 
+//loads template for supporting companies
+$("#supporting-companies").click(function(){
+    $.ajax({
+        url: "companies.html",
+        //change to false to retrieve current version of file
+        cache: true 
+    }).done(function(html){
+        $(".content-container").html(html);
+    });
+});
+
+$("#projects").click(function(){
+    $.ajax({
+        url: "projects.html", 
+        cache: true
+    }).done(function(html){
+        $(".content-container").html(html);
+    })
+});
